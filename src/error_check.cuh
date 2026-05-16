1c33bb3960dd85a13c07372cee34589edc5c4240
Jonathan Lin <jonathanlinonline@gmail.com>
2026-05-16 13:29:35 -0700

    cuda error checking

---
+++ src/error_check.cuh
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include "cuda_runtime.cuh"
+
+/// CUDA error check macro, which exists on failure
+/// https://stackoverflow.com/a/14038590
+#define checkCuda(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line)
+{
+    if (code != cudaSuccess) {
+        std::cerr << "CUDA Error: " << cudaGetErrorString(code) << " " << file << ":" << line << std::endl;
+        std::exit(1);
+    }
+}
+
+// this file is taken from Caltech's CS179 course
