ed61206e8f852fabcf07659101ae58f78c80bcf0
Jonathan Lin <jonathanlinonline@gmail.com>
2026-05-15 17:49:14 -0700

    make_sample_option_chain() docs

--- src/benchmark/sample_options.hpp
+++ src/benchmark/sample_options.hpp
@@ -5,4 +5,11 @@
 
 #include "../pricing/option.hpp"
 
+/**
+ * Generates synthetic option data for benchmarking.
+ *
+ * @param size Benchmark workload size. Must be "small", "medium", or "large".
+ * @return A vector of generated options.
+ * @throws invalid_argument if size is not one of the supported values.
+ */
 std::vector<Option> make_sample_option_chain(const std::string& size);
