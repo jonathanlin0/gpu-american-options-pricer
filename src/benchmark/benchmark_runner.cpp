ce40ba1aacb40244712dc6d42e593e24160d39a5
Jonathan Lin <jonathanlinonline@gmail.com>
2026-05-16 14:39:51 -0700

    integrate GpuPricer in BenchmarkRunner

--- src/benchmark/benchmark_runner.cpp
+++ src/benchmark/benchmark_runner.cpp
@@ -16,11 +16,13 @@ using std::endl;
 BenchmarkRunner::BenchmarkRunner(
     int warmup_runs,
     int measured_runs,
-    CpuPricer cpu_pricer
+    CpuPricer cpu_pricer,
+    GpuPricer gpu_pricer
 )
     : warmup_runs_(warmup_runs),
       measured_runs_(measured_runs),
-      cpu_pricer_(cpu_pricer)
+      cpu_pricer_(cpu_pricer),
+      gpu_pricer_(gpu_pricer)
 {
     if (warmup_runs < 0) {
         throw std::invalid_argument("warmup_runs must be non-negative");
@@ -44,21 +46,30 @@ BenchmarkResult BenchmarkRunner::run(
     // warmup
     cout << "Starting warmup..." << endl;
     for (int i = 0; i < warmup_runs_; i++) {
-        cpu_pricer_.price(options, steps);
+        cpu_pricer_.price(options, steps); // cpu warmup isn't rly necessary. maybe remove in future
+        gpu_pricer_.price(options, steps);
     }
     cout << "Finished warmup" << endl;
 
     // timed runs
     vector<float> cpu_times_ms;
+    vector<float> gpu_times_ms;
     cout << "Starting timed batch runs..." << endl;
     for (int i = 0; i < measured_runs_; i++) {
         cout << "batch " << i << "/" << measured_runs_ << endl;
-        auto start = std::chrono::steady_clock::now();
-        cpu_pricer_.price(options, steps); // can maybe save the prices in the future to check error margin
-        auto end = std::chrono::steady_clock::now();
-        float elapsed_ms = std::chrono::duration<float, std::milli>(end - start).count();
 
-        cpu_times_ms.push_back(elapsed_ms);
+        auto cpu_start = std::chrono::steady_clock::now();
+        cpu_pricer_.price(options, steps); // can maybe save the prices in the future to check error margin
+        auto cpu_end = std::chrono::steady_clock::now();
+        float cpu_elapsed_ms = std::chrono::duration<float, std::milli>(cpu_end - cpu_start).count();
+
+        auto gpu_start = std::chrono::steady_clock::now();
+        gpu_pricer_.price(options, steps); // can maybe save the prices in the future to check error margin
+        auto gpu_end = std::chrono::steady_clock::now();
+        float gpu_elapsed_ms = std::chrono::duration<float, std::milli>(gpu_end - gpu_start).count();
+
+        cpu_times_ms.push_back(cpu_elapsed_ms);
+        gpu_times_ms.push_back(gpu_elapsed_ms);
     }
     cout << "Finished timed batch runs" << endl;
 
@@ -73,16 +84,27 @@ BenchmarkResult BenchmarkRunner::run(
     ) / cpu_times_ms.size());
     float cpu_time_per_option_price_ms = cpu_batch_mean_time_ms / options.size();
 
+    float gpu_batch_mean_time_ms = accumulate(gpu_times_ms.begin(), gpu_times_ms.end(), 0.0f) / gpu_times_ms.size();
+    float gpu_batch_std_time_ms = sqrt(accumulate(
+        gpu_times_ms.begin(),
+        gpu_times_ms.end(),
+        0.0f,
+        [gpu_batch_mean_time_ms](float acc, float x) {
+            return acc + (x - gpu_batch_mean_time_ms) * (x - gpu_batch_mean_time_ms);
+        }
+    ) / gpu_times_ms.size());
+    float gpu_time_per_option_price_ms = gpu_batch_mean_time_ms / options.size();
+
     return BenchmarkResult{
         cpu_batch_mean_time_ms,
-        -1,
+        gpu_batch_mean_time_ms,
         cpu_batch_std_time_ms,
-        -1,
+        gpu_batch_std_time_ms,
         cpu_time_per_option_price_ms,
-        -1,
-        -1,
+        gpu_time_per_option_price_ms,
+        cpu_batch_mean_time_ms / gpu_batch_mean_time_ms,
         cpu_times_ms,
-        {},
+        gpu_times_ms,
         options.size(),
         steps,
     };
--- src/benchmark/benchmark_runner.hpp
+++ src/benchmark/benchmark_runner.hpp
@@ -4,11 +4,12 @@
 #include "benchmark_result.hpp"
 #include "pricing/option.hpp"
 #include "pricing/cpu_pricer.hpp"
+#include "pricing/gpu_pricer.cuh"
 
 
 class BenchmarkRunner {
 public:
-    BenchmarkRunner(int warmup_runs, int measured_runs, CpuPricer cpu_pricer);
+    BenchmarkRunner(int warmup_runs, int measured_runs, CpuPricer cpu_pricer, GpuPricer gpu_pricer);
 
     BenchmarkResult run(
         const std::vector<Option>& options,
@@ -19,5 +20,6 @@ private:
     int warmup_runs_;
     int measured_runs_;
     CpuPricer cpu_pricer_;
+    GpuPricer gpu_pricer_;
 };
 
