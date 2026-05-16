d5b6d5ba69771dbc2b88130499766c5117222d3a
Jonathan Lin <jonathanlinonline@gmail.com>
2026-05-16 14:39:14 -0700

    changed meaning of avg_gpu_speedup in BenchmarkResult

--- src/benchmark/benchmark_result.hpp
+++ src/benchmark/benchmark_result.hpp
@@ -10,7 +10,7 @@ struct BenchmarkResult {
     float gpu_batch_std_time_ms;
     float cpu_time_per_option_price_ms;
     float gpu_time_per_option_price_ms;
-    float avg_gpu_speedup; // ratio of gpu speed to cpu speed
+    float avg_gpu_speedup; // ratio of cpu speed to gpu speed
     
     std::vector<float> cpu_times_ms;
     std::vector<float> gpu_times_ms;
