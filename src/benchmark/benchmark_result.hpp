1d077c33ef2ad069522fcfb81298b483b392ee41
Jonathan Lin <jonathanlinonline@gmail.com>
2026-05-15 23:04:51 -0700

    temporarily removed p99 and prices from BenchmarkResult

--- src/benchmark/benchmark_result.hpp
+++ src/benchmark/benchmark_result.hpp
@@ -3,20 +3,15 @@
 #include <vector>
 
 struct BenchmarkResult {
-    float avg_cpu_time_ms;
-    float avg_gpu_time_ms;
-    float std_cpu_time_ms;
-    float std_gpu_time_ms;
-    float p99_cpu_time_ms;
-    float p99_gpu_time_ms;
+    float cpu_mean_time_ms;
+    float gpu_mean_time_ms;
+    float cpu_std_time_ms;
+    float gpu_std_time_ms;
     float avg_gpu_speedup; // ratio of gpu speed to cpu speed
-    float p99_gpu_speedup;
 
     std::vector<float> cpu_times_ms;
     std::vector<float> gpu_times_ms;
-    std::vector<float> cpu_prices;
-    std::vector<float> gpu_prices;
 
-    int num_options;
+    size_t num_options;
     int steps;
 };
